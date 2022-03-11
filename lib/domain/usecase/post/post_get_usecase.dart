978092ac99d62c7be7fe934d29df605609f96292
Saurabh kumar <er.sorbh@gmail.com>
2022-03-11 15:18:33 +0530

    - get post query feature

---
+++ lib/domain/usecase/post/post_get_usecase.dart
@@ -0,0 +1,22 @@
+import 'package:amity_sdk/core/core.dart';
+import 'package:amity_sdk/core/model/api_request/get_post_request.dart';
+import 'package:amity_sdk/domain/domain.dart';
+
+class PostGetUsecase extends UseCase<AmityPost, GetPostRequest> {
+  final PostRepo postRepo;
+  final PostComposerUsecase postComposerUsecase;
+  PostGetUsecase({required this.postRepo, required this.postComposerUsecase});
+
+  @override
+  Future<AmityPost> get(GetPostRequest params) async {
+    throw UnimplementedError();
+    // final amityPost = await postRepo.createPost(params);
+    // final amityComposedPost = await postComposerUsecase.get(amityPost);
+    // return amityComposedPost;
+  }
+
+  @override
+  Stream<AmityPost> listen(GetPostRequest params) {
+    throw UnimplementedError();
+  }
+}
