c654b43c3d5d2846ad64dcd549951fd1e33981bd
Trust Ratchasan <trustrat@gmail.com>
2022-03-29 15:18:43 +0200

    Add register/unregister UI in sample app

--- lib/domain/usecase/notification/register_device_notification_usecase.dart
+++ lib/domain/usecase/notification/register_device_notification_usecase.dart
@@ -5,6 +5,7 @@ import 'package:amity_sdk/core/usercase/usecase.dart';
 import 'package:amity_sdk/data/data_source/data_source.dart';
 import 'package:amity_sdk/domain/domain.dart';
 import 'package:amity_sdk/domain/repo/notification_repo.dart';
+import 'dart:io' show Platform;
 
 class RegisterDeviceNotificationUseCase extends UseCase<void, String> {
   final NotificationRepo notificationRepo;
@@ -19,12 +20,20 @@ class RegisterDeviceNotificationUseCase extends UseCase<void, String> {
     if (account.userId == null || account.deviceId == null) {
       throw NullThrownError();
     }
+    var platform = "";
+    if (Platform.isAndroid) {
+      platform = "android";
+    } else if (Platform.isIOS) {
+      platform = "iOS";
+    } else {
+      throw NullThrownError();
+    }
     final RegisterNotificationRequest request = RegisterNotificationRequest(
         userId: account.userId!,
         deviceId: account.deviceId!,
         token: params,
         provider: "fcm",
-        platform: "flutter");
+        platform: platform);
     return notificationRepo.registerNotification(request);
   }
 
