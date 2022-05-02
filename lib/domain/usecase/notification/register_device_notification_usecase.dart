40649a4ae68a59a3b582c788e08a9e0aff5085b8
Saurabh kumar <er.sorbh@gmail.com>
2022-05-02 14:53:10 +0530

    fix: fix ios push notification

--- lib/domain/usecase/notification/register_device_notification_usecase.dart
+++ lib/domain/usecase/notification/register_device_notification_usecase.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
+import 'dart:io' show Platform;
 
 import 'package:amity_sdk/core/model/api_request/register_notification_request.dart';
 import 'package:amity_sdk/core/usercase/usecase.dart';
 import 'package:amity_sdk/data/data_source/data_source.dart';
 import 'package:amity_sdk/domain/domain.dart';
 import 'package:amity_sdk/domain/repo/notification_repo.dart';
-import 'dart:io' show Platform;
 
 class RegisterDeviceNotificationUseCase extends UseCase<void, String> {
   final NotificationRepo notificationRepo;
@@ -24,7 +24,7 @@ class RegisterDeviceNotificationUseCase extends UseCase<void, String> {
     if (Platform.isAndroid) {
       platform = "android";
     } else if (Platform.isIOS) {
-      platform = "iOS";
+      platform = "ios";
     } else {
       throw NullThrownError();
     }
