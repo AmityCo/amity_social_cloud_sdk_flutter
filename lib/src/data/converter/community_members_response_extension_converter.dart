d0dea0e268f4d6520dca2dc78483de032b1bce05
Trust Ratchasan <trustrat@gmail.com>
2022-05-21 08:47:07 +0200

    fix: remove unused function in response extension

--- lib/src/data/converter/community_members_response_extension_converter.dart
+++ lib/src/data/converter/community_members_response_extension_converter.dart
@@ -9,44 +9,4 @@ extension CommunityMemmbersResponseExtension on GetCommunityMembersResponse {
         ..id = '$hashCode'
         ..communityMemberIds = communityUsers.map((e) => e.userId).toList()
         ..nextToken = paging?.next;
-
-  Future saveToDb(DbAdapterRepo dbRepo) async {
-    //Convert to File Hive Entity
-    //we have save the file first, since every object depends on file
-    List<FileHiveEntity> fileHiveEntities =
-        files.map((e) => e.convertToFileHiveEntity()).toList();
-
-    //Convert to Community Hive Entity
-    List<CommunityHiveEntity> communityHiveEntities =
-        communities.map((e) => e.convertToCommunityHiveEntity()).toList();
-
-    //Convert to User Hive Entity
-    List<UserHiveEntity> userHiveEntities =
-        users.map((e) => e.convertToUserHiveEntity()).toList();
-
-    //Convert to Community Member Hive Entity
-    List<CommnityMemberHiveEntity> communityMemberHiveEntities = communityUsers
-        .map((e) => e.convertToCommnityMemberHiveEntity())
-        .toList();
-
-    //Save the File Entity
-    for (var e in fileHiveEntities) {
-      await dbRepo.fileDbAdapter.saveFileEntity(e);
-    }
-
-    //Save the Community Entity
-    for (var e in communityHiveEntities) {
-      await dbRepo.communityDbAdapter.saveCommunityEntity(e);
-    }
-
-    //Save the User Entity
-    for (var e in userHiveEntities) {
-      await dbRepo.userDbAdapter.saveUserEntity(e);
-    }
-
-    //Save the Community Member Entity
-    for (var e in communityMemberHiveEntities) {
-      await dbRepo.communityMemberDbAdapter.saveCommunityMemberEntity(e);
-    }
-  }
 }
