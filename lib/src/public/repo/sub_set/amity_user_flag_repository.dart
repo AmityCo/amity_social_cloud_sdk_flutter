f5ab2f7cc200339d8c30bbf0cd23fba580d28abd
Saurabh kumar <er.sorbh@gmail.com>
2023-03-16 14:11:27 +0700

    chore: Added auto sps ids for user_repo

--- lib/src/public/repo/sub_set/amity_user_flag_repository.dart
+++ lib/src/public/repo/sub_set/amity_user_flag_repository.dart
@@ -9,11 +9,19 @@ class AmityUserFlagRepository {
   /// Init Amity User Flag Repo
   AmityUserFlagRepository({required this.userId});
 
+  /* begin_public_function 
+  id: user.flag
+  */
   Future<AmityUser> flag() {
     return serviceLocator<UserFlagUsecase>().get(userId);
   }
+  /* end_public_function */
 
+  /* begin_public_function 
+  id: user.unflag
+  */
   Future<AmityUser> unflag() {
     return serviceLocator<UserUnflagUsecase>().get(userId);
   }
+  /* end_public_function */
 }
