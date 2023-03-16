0a6ff5741c70b32150279614228dc3658c453a73
Saurabh kumar <er.sorbh@gmail.com>
2023-03-16 14:33:27 +0700

    chore: updated auto sps ids

--- lib/src/public/repo/post_repository.dart
+++ lib/src/public/repo/post_repository.dart
@@ -6,30 +6,43 @@ import 'package:amity_sdk/src/public/public.dart';
 
 /// Post Repo to get [AmityPost]
 class PostRepository {
+  /* begin_public_function 
+  id: post.get
+  */
   /// get composed [AmityPost] for the psot
   Future<AmityPost> getPost(String postId) {
     return serviceLocator<GetPostByIdUseCase>().get(postId);
   }
+  /* end_public_function */
 
+  /* begin_public_function 
+  id: post.query
+  */
   /// Query [AmityPost] for the user with query builder
   AmityPostGetTargetSelector getPosts() {
     return AmityPostGetTargetSelector(useCase: serviceLocator());
   }
+  /* end_public_function */
 
   /// listen to [AmityPost] with post id
   StreamController<AmityPost> getPostStream(String postId) {
     return serviceLocator<GetPostByIdUseCase>().listen(postId);
   }
+  /* end_public_function */
 
   /// Create Text/Image/Video/File post and retune [AmityPost]
   AmityPostCreateTargetSelector createPost() {
     return AmityPostCreateTargetSelector(useCase: serviceLocator());
   }
 
+  /* begin_public_function 
+  id: post.soft_delete
+  */
   /// delete the [AmityPost]
   Future deletePost({required String postId, bool hardDelete = false}) {
     return serviceLocator<PostDeleteUseCase>().get(postId);
   }
+  /* end_public_function */
 
   /// Review the [AmityPost], Accept/denied
   PostReviewQueryBuilder reviewPost({required String postId}) {
