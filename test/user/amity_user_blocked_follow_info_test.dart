a46b7be3d8d6edf4ee799904f8e5d2bad0a66ac1
Saurabh kumar <er.sorbh@gmail.com>
2023-04-18 16:25:42 +0530

    test: [ASC-12233] Added test-case for follow info status blocked

---
+++ test/user/amity_user_blocked_follow_info_test.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:amity_sdk/src/src.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../helper/amity_core_client_mock_setup.dart';
+
+class MockFollowApiInterface extends Mock implements FollowApiInterface {}
+
+// integration_test_id:09d150b5-350e-46de-8c0d-548b497ee01c
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  String userId = 'victimAndroid';
+
+  final followApiInterface = MockFollowApiInterface();
+
+  setUpAll(() async {
+    await AmityCoreClientMockSetup.setup();
+    await AmityCoreClientMockSetup.loadMockSession();
+
+    serviceLocator.registerLazySingleton<FollowApiInterface>(
+      () => followApiInterface,
+    );
+  });
+
+  test('When user queries follow info of blocked user, its follow status should be changed to blocked', () async {
+    when(() => followApiInterface.getFollowInfo(any())).thenAnswer((_) async {
+      final response = await File('test/mock_json/amity_user_blocked_follow_info.json').readAsString();
+      return FollowInfoResponse.fromJson(json.decode(response));
+    });
+
+    final amityUserFollowInfo = await AmityCoreClient.newUserRepository().relationship().user(userId).getFollowInfo();
+
+    expect(amityUserFollowInfo, isA<AmityUserFollowInfo>());
+    expect(amityUserFollowInfo.status, AmityFollowStatus.BLOCKED);
+  });
+
+  tearDownAll(() async {
+    await AmityCoreClient.logout();
+  });
+}
--- test/user/amity_user_get_blocked_user_empty_test.dart
+++ test/user/amity_user_get_blocked_user_empty_test.dart
@@ -24,11 +24,12 @@ void main() {
     serviceLocator.registerLazySingleton<UserApiInterface>(
       () => userApiInterface,
     );
+    registerFallbackValue(OptionsRequest());
   });
 
   test('When user queries for list of blocked users when none of them are blocked, it should return empty list',
       () async {
-    when(() => userApiInterface.getBlockedUsers()).thenAnswer((_) async {
+    when(() => userApiInterface.getBlockedUsers(any())).thenAnswer((_) async {
       final response = await File('test/mock_json/amity_user_get_blocked_user_empty.json').readAsString();
       return UsersResponse.fromJson(json.decode(response));
     });
